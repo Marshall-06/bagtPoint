d33d91c514d0fb02c264b19284d0b408668ed178
Marshall-06 <you@example.com>
2026-04-10 17:32:12 +0500

    fixing the cors

--- cmd/index.js
+++ cmd/index.js
@@ -8,10 +8,14 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("../src/config/swagger");
 
 app.use(cors({
-  origin: "*",
+  origin: true,
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
-}))
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"]
+}));
+
+app.options("*", cors());
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
